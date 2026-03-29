941149f44a6066efe1073223e3c48ae1f98e9697
aryannema <aaryan.techy@gmail.com>
2026-03-29 20:38:39 +0530

    refactor: set up Apollo Server with express middleware and update user resolver

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,9 +1,13 @@
+import { argsToArgsConfig } from "graphql/type/definition.js";
 import { users } from "../dummyData/data.js";
 
 const userResolver = {
   Query: {
     users: () => {
       return users;
+    },
+    user: (_, { userId }) => {
+      return users.find((user) => user._id === userId);
     },
   },
   Mutation: {},
